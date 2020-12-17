e2189e0aad0a865fac8ea37aa46ba019d0cb4653
Alex Ligo <alexandrekl@hotmail.com>
2020-12-17 08:43:37 -0500

    penetration analysis with US Census BLS data, 2-digit NAICS

--- loans_BLS.py
+++ loans_BLS.py
@@ -10,9 +10,20 @@ import glob
 import pandas as pd
 import zipfile
 from plotnine import *    # python lib to use ggplot
+import numpy as np
 
 NEstates = ['CT','MA','ME','NH','RI','VT']
 NEstfips = ['09','25','23','33','44','50']
+
+def OverrideNAICS2(df):
+    # adjusts 2-digit NAICS that are joint, e.g. NAICS 31-33 Manufacturing
+    df.loc[df['NAICS2'].eq('31'),'NAICS2'] = '31-33'
+    df.loc[df['NAICS2'].eq('33'),'NAICS2'] = '31-33'
+    df.loc[df['NAICS2'].eq('44'),'NAICS2'] = '44-45' # NAICS 44-45 Retail trade
+    df.loc[df['NAICS2'].eq('45'),'NAICS2'] = '44-45' # NAICS 44-45 Retail trade
+    df.loc[df['NAICS2'].eq('48'),'NAICS2'] = '48-49' # 	NAICS 48-49 Transportation and warehousing
+    df.loc[df['NAICS2'].eq('49'),'NAICS2'] = '48-49' # 	NAICS 48-49 Transportation and warehousing
+    return df
 
 # read PPP loan data
 fpath = '/Users/aligo/Downloads/FEMA recovery data/PPP_loans_from_SBA/120120 Paycheck Protection Program Data/'
@@ -27,10 +38,11 @@ for csv_file in csv_files:
     list_df.append(df)
 
 df = pd.concat(list_df)
-df = df.assign( NAICS3 = df.NAICSCode.str.slice(start=0, stop=3))
+df = df.assign( NAICS2 = df.NAICSCode.str.slice(start=0, stop=2))
 
 # keep loan data for New England only
-loansNE = df[df.Zip.notna() & df.State.isin(NEstates)]
+loansNE = df[df.Zip.notna() & df.State.isin(NEstates)
+             & ~df.NAICSCode.isna()]   # only non-empty NAICS codes
 loansNE = loansNE.set_index('Zip')
 
 # list business types
@@ -67,24 +79,24 @@ zipcounty = zipcounty[['ZIP','COUNTY']].reset_index().set_index('COUNTY').join(
 zipcounty.columns=['COUNTYfips','index','Zip','State','COUNTYName']
 zipcounty = zipcounty[['Zip','State','COUNTYfips','COUNTYName']].set_index('Zip')
 
-#loansc = loans.join(zipcounty, rsuffix='z').reset_index() # loans with County fips and name
-
-# Number of loans per county and NAICS3
-#loansum = loansc.groupby(['State','COUNTYfips','COUNTYName','NAICS3']).agg({'Zip':'count'}).reset_index()
-#loansum.columns=['State','COUNTYfips','COUNTYName','NAICS3','NLoans']
 loansc = loans.join(zipcounty['COUNTYfips']).reset_index() # loans with County fips and name
+# adjusts 2-digit NAICS that are joint, e.g. NAICS 31-33 Manufacturing
+loansc = OverrideNAICS2(loansc)
 
-# Number of loans per county and NAICS3
-loansum = loansc.groupby(['COUNTYfips','NAICS3']).agg({'Zip':'count'})
-loansum.columns=['NLoans']
+# Number of loans per county and NAICS2
+loansum = loansc.groupby(['COUNTYfips','NAICS2']).agg({'Zip':'count'
+                                ,'LoanAmount':'sum', 'JobsReported':'sum'})
+loansum.columns=['NLoans','TotLoanAmount','TotJobsReported']
 
 # TOTAL NUMBER OF BUSINESSES 
 # read total number of businesses from BLS
 fpath = '/Users/aligo/Downloads/FEMA recovery data/'
 zf = zipfile.ZipFile( fpath + '2020_qtrly_singlefile.zip') 
 qcew = pd.read_csv(zf.open('2020.q1-q2.singlefile.csv'), dtype={'area_fips':'object'})
-cnt = qcew[qcew.own_code.eq(5) & qcew.qtr.eq(2)  # private, 2nd quarter only
-        & qcew['industry_code'].str.len().eq(3)    # 3-digit NAICS only
+cnt = qcew[qcew.own_code.eq(5) & qcew.qtr.eq(1)  # private, FIRST quarter only
+        & (qcew['industry_code'].str.len().eq(2)   # 2-digit NAICS
+           | qcew['industry_code'].str.match('\d\d-\d\d'))    # 2-digit NAICS that are joint, e.g. NAICS 31-33 Manufacturing
+        & ~qcew['industry_code'].eq('10')      # exclude code = 10 that are "ALL Naics" totals
         & qcew['area_fips'].isin(countyfips['COUNTY'].tolist())]  # New England counties
 cnt = cnt[['area_fips','industry_code','qtrly_estabs']].set_index('area_fips')
 # Add state and county name
@@ -92,32 +104,41 @@ cnt = cnt.join(countyfips.set_index('COUNTY'))
 
 # Join Total Num businesses + Num of loans
 cnt = cnt.reset_index()
-cnt.columns = ['COUNTYfips','NAICS3','NEstabs','State','COUNTYName']
-cnt = cnt.set_index(['COUNTYfips','NAICS3'])
+cnt.columns = ['COUNTYfips','NAICS2','NEstabs','State','COUNTYName']
+cnt = cnt.set_index(['COUNTYfips','NAICS2'])
 pen = cnt.join(loansum, rsuffix='loan')
-pen = pen.assign(penetration = pen['NLoans']/pen['NEstabs'])
+pen = pen.assign(penetration = pen['NLoans']/pen['NEstabs']
+                 , AvgLoanAmount = pen['TotLoanAmount']/pen['NLoans']
+                 , AvgJobsReported = pen['TotJobsReported']/pen['NLoans']
+                 , LoanAmtperEmp = pen['TotLoanAmount']/pen['TotJobsReported'])
 
 # Add NAICS descriptions
 # naics codes downloaded from https://data.bls.gov/cew/doc/titles/industry/industry_titles.csv
 fpath = '/Users/aligo/git-repos/FEMA/BLS_data/'
 dfnaics = pd.read_csv(fpath + 'industry_titles.csv')
-dfnaics['industry_title'] = dfnaics['industry_title'].str.replace(
-                                      'NAICS \d+ ','').str.replace('\d+ ','')
-dfnaics = dfnaics[dfnaics['industry_code'].str.len().eq(3)]
-dfnaics.columns = ['NAICS3','NAICSdescr']
-pen = pen.reset_index().set_index('NAICS3').join(dfnaics.set_index('NAICS3')).reset_index()
+dfnaics = dfnaics[dfnaics['industry_code'].str.len().eq(2)
+            | dfnaics['industry_code'].str.contains('^[0-9][0-9]-[0-9][0-9]')]
+dfnaics['industry_title'] = dfnaics['industry_title'].str.replace('^NAICS \d\d ',''
+                                        ).str.replace('^NAICS \d\d-\d\d ','')
+dfnaics.columns = ['NAICS2','NAICSdescr']
+pen = pen.reset_index().set_index('NAICS2').join(dfnaics.set_index('NAICS2')).reset_index()
 
 # SAVE 
 fpath = '/Users/aligo/Downloads/FEMA recovery data/PPP_loans_from_SBA/'
 idx = pen['NLoans'].isna()
 pen.loc[idx,'NLoans'] = 0
 pen.loc[idx,'penetration'] = 0
-pen[['State','COUNTYfips','COUNTYName','NAICS3','NAICSdescr'
-     ,'NEstabs','NLoans','penetration']].to_csv(fpath + 'PPPpenetration.csv')
+pen = pen.replace([np.inf, -np.inf], np.nan)  # entries with loans but no establishments
+#pen[['State','COUNTYfips','COUNTYName','NAICS3','NAICSdescr'
+#     ,'NEstabs','NLoans','penetration']].to_csv(fpath + 'PPPpenetration.csv')
+
+with pd.ExcelWriter(fpath + 'PPPpenetrationBLS.xlsx') as writer:
+    pen[['State','COUNTYfips','COUNTYName','NAICS2','NAICSdescr','NEstabs','NLoans'
+              ,'penetration','AvgLoanAmount','AvgJobsReported','LoanAmtperEmp']
+                            ].to_excel(writer, sheet_name='PPP', index=False)
 
 # Summaries
-print('Total Number of PPP loans to New England, including all: ' + 
-                                                         str(loansNE.shape[0]))
+print('Total Number of PPP loans to New England with valid NAICS: ' + str(loansNE.shape[0]))
 print('Total Number of PPP loans to New England, excluding ' + ''.join(soleprop) 
                                                   + ': ' + str(loans.shape[0]))
 print('Previous Number excluding loans with missing info: ' 
@@ -128,15 +149,26 @@ print('Total Number of County-NAICS pairs in NE with existing businesses: '
 print('Previous Number with penetration > 1: ' + str(sum(pen['penetration'].gt(1))))
 print('Previous Number with penetration = 0: ' + str(sum(pen['penetration'].isna())))
 
+# total per county 
+dfc = pen.groupby(['COUNTYName']).agg('sum').reset_index()
+dfc['penetration'] = dfc['NLoans'] / dfc['NEstabs']
+ggplot(dfc, aes(x='reorder(COUNTYName,penetration)', y='penetration')
+          ) + geom_bar(stat="identity"
+          ) + xlab('County'
+          ) + ylab('PPP Loan Penetration'
+          ) + ggtitle('New England PPP Penetrations per County'
+          ) + theme(axis_text_y = element_text(size=6)
+          ) + coord_flip()
+
 # total per NAICS
-dfp = pen.groupby(['NAICS3','NAICSdescr']).agg('sum').reset_index()
+dfp = pen.groupby(['NAICS2','NAICSdescr']).agg('sum').reset_index()
 dfp['penetration'] = dfp['NLoans'] / dfp['NEstabs']
 
-ggplot(dfp[dfp.penetration.gt(1)], aes(x='reorder(NAICSdescr,penetration)', y='penetration')
+ggplot(dfp, aes(x='reorder(NAICSdescr,penetration)', y='penetration')
           ) + geom_bar(stat="identity"
-          ) + xlab('NAICS Three Digit Subsector'
+          ) + xlab('NAICS 2 Digit Sector'
           ) + ylab('PPP Loan Penetration'
-          ) + ggtitle('New England Overall Penetrations > 1'
+          ) + ggtitle('New England PPP Penetrations per NAICS Sector'
 #          ) + scale_y_continuous(trans = 'log2'
           ) + coord_flip()
                                    
