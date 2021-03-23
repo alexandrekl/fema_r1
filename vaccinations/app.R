5c3f7864986685e0d2731281778a782791769aa7
Alex Ligo <alexandrekl@hotmail.com>
2021-03-23 12:43:16 -0400

    commit before merging with Jeff's changes in the remote repo

--- vaccinations/app.R
+++ vaccinations/app.R
@@ -16,7 +16,7 @@ library(shiny)
 # If this variable is TRUE (FEMA Internal), shows two tabs where the first is the capacity comparison per state 
 # and the second tab is the vaccination comparison across states
 # if the variable is FALSE, only the content of the first tab is displayed
-FEMA_version <- FALSE
+FEMA_version <- TRUE
 
 theme_set( theme_bw() + theme( legend.position="bottom" ) +
                theme( legend.title=element_blank() ) )
